505953ce353c0844b53f29d57ee1995948710283
tristanhegseth <74080715+tristanhegseth@users.noreply.github.com>
2020-11-23 16:57:34 -0800

    Update finalproject.ino

--- finalproject.ino
+++ finalproject.ino
@@ -1,113 +1,189 @@
+/*
+* CPE 301 Final Project Team 5 - December 6, 2020
+* Taylor Miller, Tristan Hegseth
+* Swamp Cooler Code using Arduino Mega 2560 
+*/
+
+
+//TODO ADD MOTOR AND LED PINS
+/*
+static const int    red_led = 
+static const int yellow_led = 
+static const int  green_led = 
+static const int   blue_led = 
+
+static const int  motor_pin = 
+*/
+
+
+//TODO ADD STOP BUTTON PIN TO ANALOG PINS, NOT DIGITAL
+/*
+static const int stop_button_pin = A2;
+int stop_button_channel = stop_button_pin - 54; //only on ATmega2560 
+*/
+
+//TODO MAKE REAL TIME CLOCK
+
+
+//LCD GLOBALS
 #include <LiquidCrystal.h>
+
+static const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
+//VENT GLOBALS
 #include <Servo.h>
-#include <dht_nonblocking.h>//for temp and humidity
-#define DHT_SENSOR_TYPE DHT_TYPE_11 
 
 Servo vent; //Vent angle servo motor
-
-
- // Define Port K Register Pointers
-
-
-int vent_motor_pin = 6;
-int vent_control_pin = A1;    // select the input pin for the potentiometer
+static const int vent_motor_pin = 6;
+static const int vent_control_pin = A1;    // select the input pin for the potentiometer
 int vent_control_channel = vent_control_pin - 54; //only on ATmega2560
 int vent_input_data = 0;
 float vent_angle = 0;    // variable to store the servo position
 
 
-static const int DHT_SENSOR_PIN = 2;
-DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
-float a = 9;      //temp conversion variable a and b
-float b = 5;
+//TEMPERATURE AND HUMIDITY GLOBALS
+#include <dht_nonblocking.h>
+#define DHT_SENSOR_TYPE DHT_TYPE_11 
+
+static const int dht_sensor_pin = 2;
+DHT_nonblocking dht_sensor(dht_sensor_pin, DHT_SENSOR_TYPE);
 float temperature = 0;
 float humidity = 0;
 
-const int sensorPin = A0;
 
-int HistoryValue = 0;
-char printBuffer[128];// to print water level to serial moniter
+//WATER LEVEL GLOBALS
+static const int water_sensor_pin = A0;
+int water_sensor_channel = water_sensor_pin - 54; //only on ATmega2560
+int water_level = 0;
+char water_print_buffer[128];
 
-int  temp=0;
-int Humidity=0;
 
-const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12;
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+//STATE GLOBAL
+char* state = "IDLE";
 
+
+//ARDUINO MAIN FUNCTIONS
 void setup() {
-
-  lcd.begin (16, 2); 
-  Serial.begin(9600);
+  lcd.begin (16, 2); //(Columns, Rows)
 
   adc_init(); //Enable Analog to Digital Read
+ 
   vent.attach(vent_motor_pin); //Servo pin initializatio  
 
-   //  pinMode (1, INPUT_PULLUP); //pushbutton
+  Serial.begin(9600);
 }
 
 void loop() {
-
-
-  vent_input_data = adc_read(vent_control_channel); //read from ADC channel
-  vent_angle = vent_input_data / 1000.f * 180.f; //Input ranges [0, 1000] -> Servo ranges [0, 180] conversion
-  vent.write(vent_angle); 
-
-//prints temp and humidity to serial moniter
-  dht_sensor.measure(&temperature, &humidity);
-float   tempF = (((temperature * (a / b)) + 32)); //celcius to F conversion
-  Serial.print( "T = " );
-  Serial.print( tempF, 1 );
-  Serial.print( " deg. F, H = " );
-  Serial.print( humidity, 1 );
-  Serial.println( "%" );
- // delay(3000);
+  //Move Vent
+  poll_vent();
  
-  int value = analogRead(sensorPin);  // get adc value 
-  int diffValue = HistoryValue - value;
-if (value<100)
-{
+  if (state != "DISABLED") {
+    //Get Temperature(F) and Humidity
+    dht_sensor.measure(&temperature, &humidity);
+    temperature = convertTemperature(temperature);
+ 
+    //Get Water Level
+    int water_level = adc_read(water_sensor_channel);
+  
+    //TODO ADD STOP BUTTON HERE
+
+   //TODO ADD STATE LOGIC HERE
+   //Print timestamp of state changes
+   /*
+   if (water_level < 100) {
+     digitalWrite (5, HIGH); // turn on red LED
+     digitalWrite (4, LOW);// turn off blue light
+   }
+   else {
+     digitalWrite (5, LOW); // turn off red LED
+     digitalWrite (4, HIGH); // blue led on
+   }
+   */
+  
+  }
+ 
+ 
+ 
+ switch (state) {
+   case "IDLE":
+   //print humidity/ temp lcd
+   //print water level serial
+   //light green led
+     break;
+   case "RUNNING":
+   //print humidity/temp lcd
+   //print water level serial
+   //light blue led
+   //motor on
+     break;
+   case "DISABLED":
+   //light yellow led
+   //no prints
+     break;
+   case "ERROR":
+   //print water error lcd
+   //print water level serial
+   //light red led
+   //motor off
+     break;
+   default:
+     Serial.print("Error, unknown state");
+     break;
+ }
+}
+
+
+//PRINT FUNCTIONS
+void print_water_error_lcd() {
   lcd.setCursor (0, 0);
-  lcd.clear ();
+  lcd.clear();
   lcd.setCursor (1,0);
   lcd.write("Water level is");
   lcd.setCursor(5,1);
   lcd.write("low!");
-  digitalWrite (5, HIGH); // turn on red LED
-  digitalWrite (4, LOW);// turn off blue light
 }
- else{
 
-   lcd.setCursor (0, 0);
-      lcd.clear ();
-      lcd.write("Temp:  ");
-      lcd.print(tempF);
-       lcd.write("F  ");
-      lcd.setCursor (0, 1);
-      lcd.write("Humidity: ");
-      lcd.print(humidity);
-      lcd.write("%");
-      digitalWrite (5, LOW); // turn off red LED
-digitalWrite (4, HIGH); // blue led on
-      
- }
- //
+void print_temp_humidity_lcd() {
+  lcd.setCursor (0, 0);
+  lcd.clear ();
+  lcd.write("Temp:  ");
+  lcd.print(temperature);
+  lcd.write("F  ");
+  lcd.setCursor (0, 1);
+  lcd.write("Humidity: ");
+  lcd.print(humidity);
+  lcd.write("%");
+}
 
+void print_water_level_serial() {
+  sprintf(water_print_buffer, "Water level is %d\n", water_level);
+  Serial.print(water_print_buffer);
+}
 
-    
-// print water level to serial moniter
- 
-    if((abs(diffValue)>=10 ) || ((value == 0) &&(HistoryValue !=0)))
-    {
-      sprintf(printBuffer,"Water level is %d\n", value);
-      Serial.print(printBuffer);
-      HistoryValue = value;
-    }
-    
+void print_temp_humidity_serial() {
+  Serial.print("T = ");
+  Serial.print(temperature, 1);
+  Serial.print(" deg. F, H = ");
+  Serial.print(humidity, 1);
+  Serial.println("%");
+}
 
 
+//MISCELLANEOUS FUNCTIONS
+float convertTemperature(float c) {
+  return ((c * 1.8f) + 32.f); //Celcius to F conversion, C * 9/5 + 32
+}
 
-}// void loop
+void poll_vent() {
+  vent_input_data = adc_read(vent_control_channel); //read from ADC channel
+  vent_angle = vent_input_data * 0.18f; //Input ranges [0, 1000] -> Servo ranges [0, 180] conversion
+  vent.write(vent_angle); //Servo library function
+}
 
+
+//ANALOG TO DIGITAL FUNCTIONS
 void adc_init() {
   ADCSRA |= (0x01 << ADEN); //Enable ADC
 }
